6493de907aa646eefbfbf03989874b5e2cd82050
Olexandr Zhytenko <zhytenko@ukr.net>
2018-05-29 14:12:08 +0300

    draw graph

---
+++ draw_graph.py
@@ -0,0 +1,25 @@
+# to draw graph
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+elist = [("start", "1"),
+         ("start", "2"),
+         ("1", "2"),
+         ("2", "7"),
+         ("2", "3"),
+         ("2", "8"),
+         ("8", "9"),
+         ("1", "4"),
+         ("4", "5"),
+         ("5", "finish"),
+         ("finish", "6"),
+         ("6", "7"),
+         ("7", "8")]
+G.add_edges_from(elist)
+
+nx.draw(G, with_labels=True, font_weight='bold')
+# plt.savefig("graph.png")
+plt.show()
+
